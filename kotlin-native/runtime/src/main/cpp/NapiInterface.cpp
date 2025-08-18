ef06e5e7ac8354a87a5d888ff20344e4ef167051
linhandev <hanlin21@huawei.com>
2025-08-18 10:49:20 +0800

    chore: add license headers
    Signed-off-by: linhandev <hanlin21@huawei.com>

--- kotlin-native/runtime/src/main/cpp/NapiInterface.cpp
+++ kotlin-native/runtime/src/main/cpp/NapiInterface.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2025-2025 Huawei Technologies Co., Ltd. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
 #if KONAN_OHOS
 
 #include <cstdint>
