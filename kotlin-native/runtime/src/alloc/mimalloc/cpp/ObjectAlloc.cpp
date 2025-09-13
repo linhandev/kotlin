33a06ff9abed0cde42f437b432751dd95e7d88d8
bennyhuo <bennyhuo@tencent.com>
2025-09-13 13:15:00 +0800

    Remove duplicated functions from mimalloc alloc.

--- kotlin-native/runtime/src/alloc/mimalloc/cpp/ObjectAlloc.cpp
+++ kotlin-native/runtime/src/alloc/mimalloc/cpp/ObjectAlloc.cpp
@@ -76,11 +76,3 @@ size_t alloc::allocatedBytes() noexcept {
 extern "C" void mi_hook_allocation(size_t allocated_size) mi_attr_noexcept {
     OnMemoryAllocation(allocated_size);
 }
-
-// region Tencent Code
-void alloc::Allocator::onStartGC() noexcept {
-}
-
-void alloc::Allocator::onFinishGC() noexcept {
-}
-// endregion
