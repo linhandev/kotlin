92e85126229f5f925926d0e2623108a85c897ef5
linzexin <zexin_lin_wow@163.com>
2026-03-26 11:43:29 +0800

    fix: Submit the string ffi interface that was not submitted before
    Signed-off-by: linzexin <zexin_lin_wow@163.com>

--- kotlin-native/runtime/src/main/kotlin/kotlin/NapiInterface.kt
+++ kotlin-native/runtime/src/main/kotlin/kotlin/NapiInterface.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-@file:OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
+@file:OptIn(ExperimentalForeignApi::class)
 
 package kotlin
 
@@ -112,3 +112,27 @@ public external fun createValueFromDouble(env: NativePtr, value: Double): Native
  */
 @GCUnsafeCall("Kotlin_napi_get_value_double")
 public external fun getDoubleFromValue(env: NativePtr, value: NativePtr): Double
+
+/**
+ * String conversion: Kotlin String -> ArkTS string
+ *
+ * @param env The NAPI environment.
+ * @param value The Kotlin String value to convert.
+ * @return A NativePtr representing the ArkTS string, or a null pointer if:
+ *         - the string uses an unsupported encoding, or
+ *         - the N-API call fails (e.g., due to unsupported encoding or memory error).
+ */
+@GCUnsafeCall("Kotlin_String_toNapiValue")
+@Escapes.Nothing
+public external fun String.getNapiValue(env: NativePtr): NativePtr
+
+/**
+ * String conversion: ArkTS string -> Kotlin String
+ *
+ * @param env The NAPI environment.
+ * @param value The NativePtr(representing the ArkTS string) value to convert.
+ * @return The Kotlin String value, or a null pointer if the N-API call fails during reading.
+ */
+@GCUnsafeCall("Kotlin_napi_get_kotlin_string_utf16")
+@Escapes.Nothing
+public external fun getStringFromValue(env: NativePtr, value: NativePtr): String
