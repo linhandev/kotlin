3d1e1d69113791abaec827eb9f3e9fb4e577acae
liuhanzhao <liuhanzhao@noreply.gitcode.com>
2026-05-10 10:40:02 +0800

    fix: remove kotlin.native.release compiler options
    将-DNDEBUG编译选项移到-O2之前，并添加注释说明。
    这是因为CRT头文件在未定义NDEBUG时会破坏ALWAYS_INLINE宏，
    需要在优化选项之前定义NDEBUG以避免此问题。
    
    Cherry-picked-from: 452b4605777aad2ea293e9fc6c153bd60ffcefe2
    Original-commit-by: liuhanzhao
    Signed-off-by: liuhanzhao <747555692@qq.com>

--- kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/bitcode/CompileToBitcodePlugin.kt
+++ kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/bitcode/CompileToBitcodePlugin.kt
@@ -168,12 +168,12 @@ open class CompileToBitcodeExtension @Inject constructor(val project: Project) :
     private fun getDefaultCppFlags(target: KonanTarget): List<String> {
         return getCppGcFastpathFlags(target) + listOfNotNull(
             "-std=c++17",
+            "-DNDEBUG", // This is required because crt header file corrupts ALWAYS_INLINE without NDEBUG
             "-O2",
             "-fno-aligned-allocation", // TODO: Remove when all targets support aligned allocation in C++ runtime.
             "-Wno-unused-parameter",  // False positives with polymorphic functions.
             "-Wall",
             "-Wextra",
-            "-DNDEBUG",
         )
     }
 
